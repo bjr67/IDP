cf612dc2ac0c0b071f093b01f58c1b4cd079cb30
Benjamin Ricard <benjaminricard@Benjamins-MBP.wpa.txstate.edu>
2015-12-17 16:35:21 -0600

    27th

--- R/IDP.R
+++ R/IDP.R
@@ -55,10 +55,8 @@ netcharge <- abs(net_neg_chg - net_pos_chg)
 rh_chgcorrection <- (2.16 * n^((0.503-(0.11*log(1-fppii_avg)))) + 0.17158*netcharge - 0.07296 * 2.16 * n^(0.503-(0.11*log(1-0.012))))
 
 idpcsv <- read.csv("https://public.opencpu.org/ocpu/github/bjr67/IDP/data/idpdata.csv", sep=",")
-par(mai=rep(0.1,4), pin = c(.5,.3))
-plot(idpcsv$N,idpcsv$Rh, pch=19, xlab="N", ylab = "Rh")
-points(n,rh_chgcorrection, col="red", pch=19)
-invisible();
-
+list(
+	message= paste(fppii_avg)
+	)
 }
 
