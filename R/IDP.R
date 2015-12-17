a25b88aafebf2fcfdce60b980c40d230c3255962
Benjamin Ricard <benjaminricard@Benjamins-MacBook-Pro.local>
2015-12-17 07:21:51 -0600

    10th

--- R/IDP.R
+++ R/IDP.R
@@ -53,7 +53,9 @@ rh_uncorrected <- (2.16 * sequ_ns.length ^ ((0.503-(0.11*log(1-fppii_avg)))))
 rh_chgcorrection <- (2.16 * sequ_ns.length ^((0.503-(0.11*log(1-fppii_avg)))) + 0.17158*netcharge - 0.07296 * 2.16 * sequ_ns.length^(0.503-(0.11*log(1-0.012))))
 
 idpcsv <- read.csv("https://public.opencpu.org/ocpu/github/bjr67/IDP/data/idpdata.csv", sep=",")
-print(plot(idpcsv$N,idpcsv$Rh, pch=19, xlab="N", ylab = "Rh")+
+pl <- (plot(idpcsv$N,idpcsv$Rh, pch=19, xlab="N", ylab = "Rh")+
 par(new=T)+
 plot(n, rh_chgcorrection, col="red")+
-par(new=F))}}
+par(new=F))
+
+print(pl)}}
