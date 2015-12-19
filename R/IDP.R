b122e19e30712d3fd09aa47a326dd4c5c8519928
Benjamin Ricard <benjaminricard@Benjamins-MacBook-Pro.local>
2015-12-19 17:46:24 -0600

    100

--- R/IDP.R
+++ R/IDP.R
@@ -88,7 +88,7 @@ else if (as.name(substr(sequ_ns,i,i)) == Y){
 	col[i]="orange"}
 else if (as.name(substr(sequ_ns,i,i)) == W){
 	col[i]="orange"}
-}}}
+}}
 
 fppii_avg <- fppii/(nchar(sequ_ns))
 n <- nchar(sequ_ns)
@@ -104,7 +104,7 @@ idpfppii = c(0.489,0.458,0.283,0.45,0.328,0.335,0.363,0.402,0.378,0.353,0.399,0.
 
 idp.df = data.frame(idpn, idprh, idpnc, idpneg, idppos, idpfppii)
 if (n < 70) {nliml <- n-2}
-else if (n >=70) {nliml <- 70}
+else {nliml <- 70}
 if (n > 262) {nlimu <- n+2}
 else {nlimu <- 262}
 if (rh_chgcorrection < 22) {rhliml <- rh_chgcorrection - 2}
