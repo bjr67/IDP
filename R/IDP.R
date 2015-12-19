30b5dee5f587b207b484b9a3e8dfcf4aa80b6b9a
Benjamin Ricard <benjaminricard@Benjamins-MacBook-Pro.local>
2015-12-19 17:51:07 -0600

    101

--- R/IDP.R
+++ R/IDP.R
@@ -114,7 +114,7 @@ else {rhlimu <- 51}
 par(mfrow=c(2,1))
 plot(idp.df$idpn, idp.df$idprh, pch=19, xlab="N", ylab = "Rh", xlim = c(nliml,nlimu), ylim = c(rhliml,rhlimu))
 points(n,rh_chgcorrection, pch=19, col="red")
-plot(indexppii,indiv, xlim=c(0.5, (n+0.5)), ylim=c(0,1.05), pch=19, col = col[1:n], xlab = "Amino Acid Residue", ylab = "PPii propensity")
+plot(indexppii,indiv, xlim=c(0.5, (n+0.5)), ylim=c(0,1.05), pch=19, col = col[1:length(col)], xlab = "Amino Acid Residue", ylab = "PPii propensity")
 
 abline(h = mean(indiv), col="black")
 invisible();
