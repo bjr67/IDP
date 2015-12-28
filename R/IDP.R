b5f8e09fe0a2a00c6f7aace2dc05a0ea124d46f4
Benjamin Ricard <benjaminricard@Benjamins-MacBook-Pro.local>
2015-12-28 17:26:25 -0600

    140

--- R/IDP.R
+++ R/IDP.R
@@ -120,13 +120,13 @@ if (rh_chgcorrection > 51) {rhlimu <- rh_chgcorrection +2}
 else {rhlimu <- 51}
 
 count <- table(seqv)
+max.count <-  max(count)
 
 par(mfrow=c(3,1))
 plot(idp.df$idpn, idp.df$idprh, pch=19, xlab="N", ylab = "Rh", xlim = c(nliml,nlimu), ylim = c(rhliml,rhlimu))
 points(n,rh_chgcorrection, pch=19, col="red")
 plot(indexppii,indiv, xlim=c(0.5, (n+0.5)), ylim=c(0,1.05), pch=19, xlab = "Amino Acid Residue", ylab = "PPii propensity")
-barplot(count, xlab = "Amino Acid", ylab = "Number")
-
+barplot(count, xlab = "Amino Acid", ylab = "Number", xlim  = c(0, 20))
 #abline(h = mean(indiv), col="black")
 invisible();
 }
