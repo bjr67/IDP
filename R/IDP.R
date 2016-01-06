345d4d1cc1ee6498de1701353601543836a9f4a3
Benjamin Ricard <benjaminricard@Benjamins-MacBook-Pro.local>
2016-01-06 08:08:48 -0600

    207

--- R/IDP.R
+++ R/IDP.R
@@ -46,13 +46,13 @@ indiv[i] <- eval(as.name(substr(sequ_ns,i,i)))
 indexppii[i] <- i
 
 if (eval(as.name(substr(sequ_ns,i,i))) == 0.56){
-	net_neg_chg = net_neg_chg + 1}
+	net_neg_chg <- net_neg_chg + 1}
 else if (eval(as.name(substr(sequ_ns,i,i))) == 0.38){
-	net_neg_chg = net_neg_chg + 1}
+	net_neg_chg <- net_neg_chg + 1}
 else if (eval(as.name(substr(sequ_ns,i,i))) == 0.30){
-	net_pos_chg = net_pos_chg + 1}
+	net_pos_chg <- net_pos_chg + 1}
 else if (eval(as.name(substr(sequ_ns,i,i))) == 0.42){
-	net_pos_chg = net_pos_chg + 1}
+	net_pos_chg <- net_pos_chg + 1}
 
 }
 seqv <- c()
